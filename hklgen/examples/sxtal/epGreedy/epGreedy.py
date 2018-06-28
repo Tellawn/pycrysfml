affa45de16cafa98eb417836ab7866c0100df6ff
Telon Y <yan.telon@gmail.com>
2018-06-28 16:34:05 -0400

    Update epGreedy.py

--- hklgen/examples/sxtal/epGreedy/epGreedy.py
+++ hklgen/examples/sxtal/epGreedy/epGreedy.py
@@ -16,6 +16,8 @@ values to go to next).
 """
 
 import random
+import numpy as np
+import os
 
 #For using pycrysfml
 import fswig_hklgen as H
@@ -23,17 +25,16 @@ import hkl_model as Mod
 import sxtal_model as S
 
 
-#variables
-##epsilon = 0.1
-##hkls = []
-##used = []   #all the visited hkl values
-
 class EpsilonGreedy():
     def __init__(self, epsilon, counts, rewards):
         self.epsilon = epsilon      #chance of entering exploration
         self.counts  = counts       #number of times each place/hkl has been visited
         self.rewards = rewards      #expected reward for each possibility, associated by index
         return
+
+    def initialize(self, n_actions):
+        self.counts = np.zeros(n_actions, dtype=int)
+        self.values = np.zeros(n_actions, dtype=float)
 
     #Returns the indices of the tied states/HKL values with the best immediate reward
     def bestRewards():
@@ -48,7 +49,7 @@ class EpsilonGreedy():
         return rewardIndices
 
     #Chooses a state/HKL value to go to
-    def select_arm(self):
+    def select_action(self):
         coin = random.random()
         if coin > self.epsilon:
             #pick one of the best arms/choices and return its index
@@ -58,15 +59,55 @@ class EpsilonGreedy():
             return random.randint(len(self.rewards))
 
     #Updates the aoijfaoijfaoisfj
-    def update(self, chosenArm, reward):
-        self.counts[chosenArm] += 1
-        n = self.counts[chosenArm]
+    def update(self, chosenAction, reward):
+        self.counts[chosenAction] += 1
+        n = self.counts[chosenAction]
         #The predicted reward function is an average of all the previous rewards when
         #having visited that state/location
-        self.rewards[chosenArm] = self.rewards[chosenArm]*(n-1.0)/n + float(reward)/n
+        self.rewards[chosenAction] = self.rewards[chosenAction]*(n-1.0)/n + float(reward)/n
         return
 
+class getFit():
+##    def __init__(self, holy crap pycrysfml is hard):
+##    def fit():
+        #do pycrysfml stuff here
+    def chiSq(observed, expected):
+        chisq = 0
+        for i in range(len(observed)):
+            chisq += float(observed[i]-expected[i])**2 / expected
+        return chisq
 
-def test_algorithm(agent, actions, num_epochs, horizon):
-    chosenActions = []
+def test_algorithm(agent, actions, numEpochs, horizon):
+    chosenActions = np.zeros((numEpochs, horizon))
+    rewards = np.zeros((numEpochs, horizon))
+
+
+    for epoch in range(numEpochs):
+        totalReward = 0
+        chiSq = 0
+
+        agent.initialize(len(actions))
+
+        file = open("eGreedyResults" + t + ".txt", "w")
+        file.write("HKL Value\t\tReward\tTotalReward\tChi Squared Value\n")
+
+        for t in range(horizon):
+            
+            action = agent.select_action()
+            chosenActions[epoch, t] = action
+
+            reward = actions[action]#.fit()
+            chiSq += actions[action]#.chiSq()            
+            rewards[epoch, t] = reward
+            totalReward += reward
+            
+            agent.update(chosenAction, reward)
+            
+            file.write(chosenActions[epoch, t].replace("[","").replace("]","").replace(",",""))
+            file.write("\t\t" + str(reward) + "\t" + str(totalReward) + "\t" + str(chiSq) + "\n")
+            
+    return 
+
+
+def __main__():
     
