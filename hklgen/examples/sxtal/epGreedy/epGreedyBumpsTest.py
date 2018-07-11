ff9657837316ebec667923d176a1de245ca854e3
Tellawn <yan.telon@gmail.com>
2018-07-11 09:19:47 -0400

    Made bumps tester

--- hklgen/examples/sxtal/epGreedy/epGreedyBumpsTest.py
+++ hklgen/examples/sxtal/epGreedy/epGreedyBumpsTest.py
@@ -1,21 +1,6 @@
 """
-Filename: epGreedyAnnealing.py
-Authors: Ryan Cho and Telon Yan
-Implements a modified epsilon-greedy algorithm for pycrysfml, but can be
-used for similar scenarios. For crystallography, since one may not visit
-individual HKL values more than once, what happens is:
-We have a 1 - epsilon + k*epsilon/n chance of visiting a state/machine
-whose expected reward is highest (e.g. 0.5, there may be ties) and an
-epsilon/n chance of visiting one of the states/slot machines whose
-expected reward is not the highest (where k is the number of states with
-a tied max reward, such as 0.5, and n is the total number of possible HKL
-values to go to next).
-Code borrowed from:
-https://imaddabbura.github.io/blog/data%20science/2018/03/31/epsilon-Greedy-Algorithm.html
-https://github.com/scattering/crystal-rl
-https://github.com/scattering/pycrysfml
+This is to test for any inconsistencies in Bumps
 """
-
 import os,sys;sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 import random
 import numpy as np
@@ -33,8 +18,6 @@ import  bumps.names  as bumps
 import bumps.fitters as fitter
 from bumps.formatnum import format_uncertainty_pm
 
-#Crystal model stuff
-
 np.seterr(divide="ignore",invalid="ignore")
 
 #Set data files
@@ -50,7 +33,6 @@ wavelength, refList, sfs2, error = S.readIntFile(observedFile, kind="int", cell=
 tt = [H.twoTheta(H.calcS(crystalCell, ref.hkl), wavelength) for ref in refList]
 backg = None
 exclusions = []
-
 
 def setInitParams():
     #Make a cell
@@ -77,16 +59,6 @@ def fit(model):
     fitted = fitter.LevenbergMarquardtFit(problem)
     x, dx = fitted.solve(monitors=[monitor])
     return x, dx, problem.chisq()
-
-#Beacuse I don't actually know how to use pycrysfml and bumps yet and just want something working, here's a fakeFit method that fakes the fit() method
-def fakeFit(model):
-    #x should be the predicted hkl positon, dx is the error, and chisq is the chi squared value
-    #model should be of class "Model" in the sxtal_model program, but for now it doesn't matter
-    x = random.random()
-    dx = random.random()
-    chisq = random.random()*150
-    return x, dx, chisq
-
 
 class EpsilonGreedy():
     
@@ -136,30 +108,9 @@ class EpsilonGreedy():
         return maxIndices
     #Chooses an HKL value to go to using bestReward, ignoring hkls already visited. Returns the index of the hkl chosen
     def select_action(self):
-        coin = random.random()
-        choice = 0
-        
-        #Exploit - Pick among the options (tied) with the best expected reward
-        if coin > self.epsilon:
-#            prin)
-            choice = random.choice(self.bestReward())
-            self.visited.append(choice)
-            self.visited.sort()
-            
-        #Explore - Pick a choice at random
-        else:
-#            print
-            choices = list(self.values)
-            choices_indices = []
-            for i in range(len(self.values)):
-                choices_indices.append(i)
-            for i in self.visited:
-                choices_indices.remove(i)
-                
-            choice = random.choice(choices_indices)
-            self.visited.append(choice)
-            self.visited.sort()
-            
+        choice = len(self.visited)
+        self.visited.append(choice)
+        self.visited.sort()
         return int(choice)
 
     #Updates the counts of hkls visited and their expected reward with new data
@@ -173,22 +124,6 @@ class EpsilonGreedy():
 	self.epsilon = 1 / np.log(t + 0.0000001)
         return
 
-def sorter(model, actionIndexList):
-    numActions = len(actionIndexList)
-
-    refListSorted = np.zeros(numActions)
-    errorSorted = np.zeros(numActions)
-    ttSorted = np.zeros(numActions)
-    intensitiesSorted = np.zeros(numActions)
-
-    refListSort = refList
-    model.refList = refList
-#    for i in range(numActions):
-#	refListSorted[actionIndexList[i]] = 
-#	errorSorted[actionIndexList[i]] = 
-
-#agent is the EpsilonGreedy() object, actions is a list of HKLs 
-#(each element of the list is a length 3 list: [h, k, l]), num_sims is an int, horizon is an int
 def test_algorithm(agent, actions, num_sims, horizon, numParameters):
 #    zInit = 0.3
     for simulation in range(num_sims):
@@ -269,17 +204,20 @@ def test_algorithm(agent, actions, num_sims, horizon, numParameters):
 	    if (((t > 10) and chiSq < 1.5) or (t > 100)):
 		break
 
-	if (simulation % 10 == 0):
-	    file2 = open("Rewards" + str(simulation) + ".txt", "w")
-	    file2.write("Number of epochs: " + str(simulation))
-	    np.savetxt("Rewards" + str(simulation) + ".txt", agent.values)
-	    file2.close()
+#	if (simulation % 10 == 0):
+#	    file2 = open("Rewards" + str(simulation) + ".txt", "w")
+#	    file2.write("Number of epochs: " + str(simulation))
+#	    np.savetxt("Rewards" + str(simulation) + ".txt", agent.values)
+#	    file2.close()
 
 	#Observed sfs2 values (
-	x1 = sfs2
-	y = H.calcStructFact() #model.theory()
+	x1 = sfs2[0:4]
+	y = model.theory() #H.calcstructfact()
+	print(y)
+	print(x1)
 	plt.scatter(qSquared,y)
 	plt.savefig("Calc sfs2 vs Qsq " + str(simulation) + ".png") 
+	plt.figure()
 	plt.scatter(qSquared,x1)
 	plt.savefig("Obs sfs2 vs Qsq " + str(simulation) + ".png")
 
@@ -287,29 +225,10 @@ def test_algorithm(agent, actions, num_sims, horizon, numParameters):
 
 #	zInit = model.atomListModel.atomModels[0].z.value
         file.close()
-
+        
     return
 
-#def __main__():
-#for i in refList:
-#    for j in i:
-#        print(j)
-
-
-#x2 = sfs2#
-
-#model1 = setInitParams()
-#model1.refList = H.ReflectionList(refList)
-#model1._set_reflections()
-#model1.error = error
-#model1.tt  = tt
-#model1._set_observations(sfs2)
-
-#y1 = model1.theory()
-
-#plt.scatter(x2,y1)
-#plt.savefig('sfs2stest.png') 
-
 agent = EpsilonGreedy(1, np.zeros(len(refList)), np.ones(len(refList)))
-test_algorithm(agent, refList, 1, len(refList), 1)
+#test_algorithm(agent, refList, 1, len(refList), 1)
+test_algorithm(agent, refList, 1, 4, 1)
 print("done")
